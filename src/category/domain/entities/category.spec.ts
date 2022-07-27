ef1dc9fef3a3afd7c8853f648373b548978c8a61
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-26 22:54:03 -0300

    [:construction:] category entity unit test development

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,16 @@
 import { Category } from './category';
 
 describe('[Unit] Category Entity', () => {
-  it('should be able to create a category instance', () => {
-    const category = new Category('Test');
-    expect(category.name).toBe('Test');
+  it('should be able to create a category instance using all parameters', () => {
+    const input = {
+      name: 'Test Category',
+      description: 'Test Category Description',
+      is_active: true,
+      created_at: new Date(),
+    };
+
+    const output = new Category(input);
+
+    expect(output.props).toStrictEqual(input);
   });
 });
