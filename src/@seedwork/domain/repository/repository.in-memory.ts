026e73d3bd683439afbf1a48f51f72dcd09338ab
Ayrton Souza <ayrtonsouza@icloud.com>
2022-08-08 23:01:36 -0300

    [:construction:] creating in memory repository abstract class

---
+++ src/@seedwork/domain/repository/repository.in-memory.ts
@@ -0,0 +1,44 @@
+import { Entity } from '../entities/entity';
+import { NotFoundError } from '../errors/not-found.error';
+import { UniqueEntityId } from '../value-objects/unique-entity-id';
+import { IRepository } from './repository-contracts';
+
+export abstract class RepositoryInMemory<E extends Entity>
+  implements IRepository<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  async findById(id: string | UniqueEntityId): Promise<E> {
+    const _id = `${id}`;
+    return this._get(_id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+    const index = this.items.findIndex((item) => item.id === entity.id);
+    this.items[index] = entity;
+  }
+
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    const _id = `${id}`;
+    await this._get(_id);
+    const index = this.items.findIndex((item) => item.id === _id);
+    this.items.splice(index, 1);
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const item = this.items.find((item) => item.id === id);
+    if (!item) {
+      throw new NotFoundError(`Item with id ${id} not found`);
+    }
+    return item;
+  }
+}
