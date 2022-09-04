3e56f10929f8b42ec6be80a990896dd428ada966
Ayrton Souza <ayrtonsouza@icloud.com>
2022-09-04 03:34:41 -0300

    [:white_check_mark:] creating tests to create category use case

---
+++ src/category/application/use-cases/__tests__/create-category.use-case.spec.ts
@@ -0,0 +1,60 @@
+import { CategoryInMemoryRepository } from '../../../infra/repository/category.in-memory.repository';
+import { CreateCategoryUseCase } from '../create-category.use-case';
+
+describe('[Unit] Create Category Use Case', () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  it('[method] should be able to create a category with name parameter', async () => {
+    const spyOnInsert = jest.spyOn(repository, 'insert');
+    const output = await useCase.execute({ name: 'Category 1' });
+
+    expect(output).toStrictEqual({
+      id: repository.items[0].id,
+      name: 'Category 1',
+      description: null,
+      is_active: true,
+      created_at: repository.items[0].created_at,
+    });
+    expect(spyOnInsert).toBeCalledTimes(1);
+  });
+
+  it('[method] should be able to create a category with name parameter and description', async () => {
+    const spyOnInsert = jest.spyOn(repository, 'insert');
+    const output = await useCase.execute({
+      name: 'Category 1',
+      description: 'Category 1 description',
+    });
+
+    expect(output).toStrictEqual({
+      id: repository.items[0].id,
+      name: 'Category 1',
+      description: 'Category 1 description',
+      is_active: true,
+      created_at: repository.items[0].created_at,
+    });
+    expect(spyOnInsert).toBeCalledTimes(1);
+  });
+
+  it('[method] should be able to create a category with name parameter and is_active', async () => {
+    const spyOnInsert = jest.spyOn(repository, 'insert');
+    const output = await useCase.execute({
+      name: 'Category 1',
+      is_active: false,
+    });
+
+    expect(output).toStrictEqual({
+      id: repository.items[0].id,
+      name: 'Category 1',
+      description: null,
+      is_active: false,
+      created_at: repository.items[0].created_at,
+    });
+    expect(spyOnInsert).toBeCalledTimes(1);
+  });
+});
