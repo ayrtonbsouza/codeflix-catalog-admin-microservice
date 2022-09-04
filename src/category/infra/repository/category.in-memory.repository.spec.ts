da72b17abbd8546c99442447797963bc52fb9fdc
Ayrton Souza <ayrtonsouza@icloud.com>
2022-09-04 02:45:32 -0300

    [:white_check_mark:] creating tests to category in memory repository

---
+++ src/category/infra/repository/category.in-memory.repository.spec.ts
@@ -0,0 +1,75 @@
+import { Category } from '../../domain/entities/category';
+import { CategoryInMemoryRepository } from './category.in-memory.repository';
+
+describe('[Unit] Category In Memory Repository', () => {
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+  });
+
+  it('[method] should not filter any field when filter object is null', async () => {
+    const items = [
+      new Category({ name: 'Test Category 1' }),
+      new Category({ name: 'Test Category 2' }),
+    ];
+    repository['items'] = items;
+
+    const spyOnFilter = jest.spyOn(items, 'filter' as any);
+    const filteredItems = await repository['applyFilter'](items, null);
+
+    expect(spyOnFilter).not.toHaveBeenCalled();
+    expect(filteredItems).toEqual(items);
+  });
+
+  it('[method] should be able to filter by name', async () => {
+    const items = [
+      new Category({ name: 'Test Category 1' }),
+      new Category({ name: 'Test Category 2' }),
+    ];
+    repository['items'] = items;
+
+    const spyOnFilter = jest.spyOn(items, 'filter' as any);
+    const filteredItems = await repository['applyFilter'](
+      items,
+      'Test Category 1'
+    );
+
+    expect(spyOnFilter).toHaveBeenCalled();
+    expect(filteredItems).toEqual([items[0]]);
+  });
+
+  it('[method] should be able to sort by created_at when sort param is null', async () => {
+    const created_at = new Date();
+    const items = [
+      new Category({ name: 'Test Category 1', created_at }),
+      new Category({
+        name: 'Test Category 2',
+        created_at: new Date(created_at.getTime() + 100),
+      }),
+      new Category({
+        name: 'Test Category 3',
+        created_at: new Date(created_at.getTime() + 200),
+      }),
+    ];
+    repository['items'] = items;
+
+    const sortedItems = await repository['applySort'](items, null, null);
+    expect(sortedItems).toEqual([items[2], items[1], items[0]]);
+  });
+
+  it('[method] should be able to sort by name', async () => {
+    const items = [
+      new Category({ name: 'B' }),
+      new Category({ name: 'C' }),
+      new Category({ name: 'A' }),
+    ];
+    repository['items'] = items;
+
+    let sortedItems = await repository['applySort'](items, 'name', 'asc');
+    expect(sortedItems).toEqual([items[2], items[0], items[1]]);
+
+    sortedItems = await repository['applySort'](items, 'name', 'desc');
+    expect(sortedItems).toEqual([items[1], items[0], items[2]]);
+  });
+});
--- src/category/infra/repository/category.in-memory.repository.ts
+++ src/category/infra/repository/category.in-memory.repository.ts
@@ -1,11 +1,14 @@
 import { CategoryRepository } from 'category/domain/repository/category.repository';
-import { SearchableRepositoryInMemory } from '@seedwork/domain/repository/searchableRepository.in-memory';
+import { SearchableRepositoryInMemory } from '../../../@seedwork/domain/repository/searchableRepository.in-memory';
 import { Category } from '../../domain/entities/category';
+import { SortDirection } from '../../../@seedwork/domain/repository/repository-contracts';
 
 export class CategoryInMemoryRepository
   extends SearchableRepositoryInMemory<Category>
   implements CategoryRepository.Repository
 {
+  sortableFields: string[] = ['name', 'created_at'];
+
   protected async applyFilter(
     items: Category[],
     filter: CategoryRepository.Filter
@@ -17,5 +20,15 @@ export class CategoryInMemoryRepository
     return items.filter((item) =>
       item.props.name.toLowerCase().includes(filter.toLowerCase())
     );
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string | null,
+    sort_direction: SortDirection | null
+  ): Promise<Category[]> {
+    return !sort
+      ? super.applySort(items, 'created_at', 'desc')
+      : super.applySort(items, sort, sort_direction);
   }
 }
