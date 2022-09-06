0b1dd211b2611f2bc30d5c034af9e6eb04dc9f9f
Ayrton Souza <ayrtonsouza@icloud.com>
2022-09-06 17:01:53 -0300

    [:construction:] creating update category use case

---
+++ src/category/application/use-cases/update-category.use-case.ts
@@ -0,0 +1,33 @@
+import { CategoryRepository } from '../../domain/repository/category.repository';
+import { IUseCase } from '../../../@seedwork/application/use-case';
+import { CategoryOutput, CategoryOutputMapper } from '../dtos/category-output';
+
+export type Input = {
+  id: string;
+  name: string;
+  description?: string;
+  is_active?: boolean;
+};
+
+export type Output = CategoryOutput;
+
+export class UpdateCategoryUseCase implements IUseCase<Input, Output> {
+  constructor(private categoryRepository: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const category = await this.categoryRepository.findById(input.id);
+    category.update(input.name, input.description);
+
+    if (input.is_active === true) {
+      category.activate();
+    }
+
+    if (input.is_active === false) {
+      category.deactivate();
+    }
+
+    await this.categoryRepository.update(category);
+
+    return CategoryOutputMapper.toOutput(category);
+  }
+}
