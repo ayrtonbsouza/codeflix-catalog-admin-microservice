eb1d1864b0704d1009bee3f07e01053f09dce825
Ayrton Souza <ayrtonsouza@icloud.com>
2022-09-09 22:44:25 -0300

    [:art:] adding eslint configuration to nestjs module

--- src/nestjs/.eslintrc.js
+++ src/nestjs/.eslintrc.js
@@ -5,10 +5,17 @@ module.exports = {
     tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'prettier'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'airbnb-base',
+    'plugin:import/recommended',
+    'prettier'
   ],
   root: true,
   env: {
@@ -21,5 +28,49 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'camelcase': 'off',
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'ts': 'never',
+        'js': 'never'
+      }
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': [
+          'PascalCase'
+        ],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-restricted-syntax': 'off',
+    'dot-notation': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'no-await-in-loop': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'lines-between-class-members': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'max-classes-per-file': 'off',
+    'prettier/prettier': 'error'
   },
+  settings: {
+    'import/resolver': {
+      'typescript': {}
+    }
+  }
 };
