829bf84911f56c88f80876d47528688c84a7d14a
Ayrton Souza <ayrtonsouza@icloud.com>
2022-09-11 01:05:51 -0300

    [:white_check_mark:] creating missing unit tests on category controller

--- src/nestjs/src/categories/categories.controller.spec.ts
+++ src/nestjs/src/categories/categories.controller.spec.ts
@@ -1,6 +1,13 @@
-import { CreateCategoryUseCase } from '@cam/core/category/application';
+import { SortDirection } from '@cam/core/@seedwork/domain';
+import {
+  CreateCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from '@cam/core/category/application';
 import { CategoriesController } from './categories.controller';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 describe('[Unit] Categories Controller', () => {
   let controller: CategoriesController;
@@ -20,11 +27,11 @@ describe('[Unit] Categories Controller', () => {
     const mockCreateUseCase = {
       execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
     };
-
+    // @ts-ignore
     controller['createCategoryUseCase'] = mockCreateUseCase;
     const input: CreateCategoryDto = {
-      name: 'Movie',
-      description: 'some description',
+      name: 'Category',
+      description: 'Category description',
       is_active: true,
     };
     const output = await controller.create(input);
@@ -32,19 +39,96 @@ describe('[Unit] Categories Controller', () => {
     expect(expectedOutput).toStrictEqual(output);
   });
 
-  it('[search] should be able to search categories', () => {
-    expect(true).toBeTruthy();
+  it('[search] should be able to search categories', async () => {
+    const expectedOutput: ListCategoriesUseCase.Output = {
+      items: [
+        {
+          id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+          name: 'Category',
+          description: 'Category description',
+          is_active: true,
+          created_at: new Date(),
+        },
+      ],
+      current_page: 1,
+      last_page: 1,
+      per_page: 1,
+      total: 1,
+    };
+
+    const mockListUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+
+    // @ts-ignore
+    controller['listCategoriesUseCase'] = mockListUseCase;
+    const input = {
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_direction: 'desc' as SortDirection,
+      filter: 'Category',
+    };
+
+    const output = await controller.search(input);
+    expect(mockListUseCase.execute).toHaveBeenCalledWith(input);
+    expect(expectedOutput).toStrictEqual(output);
   });
 
-  it('[findOne] should be able to get a category', () => {
-    expect(true).toBeTruthy();
+  it('[findOne] should be able to get a category', async () => {
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+    const expectedOutput: GetCategoryUseCase.Output = {
+      id,
+      name: 'Category',
+      description: 'Category description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockGetUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    // @ts-ignore
+    controller['getCategoryUseCase'] = mockGetUseCase;
+    const output = await controller.findOne(id);
+    expect(mockGetUseCase.execute).toHaveBeenCalledWith({ id });
+    expect(expectedOutput).toStrictEqual(output);
   });
 
-  it('[update] should be able to update a category', () => {
-    expect(true).toBeTruthy();
+  it('[update] should be able to update a category', async () => {
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+    const expectedOutput: UpdateCategoryUseCase.Output = {
+      id,
+      name: 'Category',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockUpdateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    // @ts-ignore
+    controller['updateCategoryUseCase'] = mockUpdateUseCase;
+    const input: UpdateCategoryDto = {
+      name: 'Category',
+      description: 'Category description',
+      is_active: true,
+    };
+    const output = await controller.update(id, input);
+    expect(mockUpdateUseCase.execute).toHaveBeenCalledWith({ id, ...input });
+    expect(expectedOutput).toStrictEqual(output);
   });
 
-  it('[remove] should be able to delete a category', () => {
-    expect(true).toBeTruthy();
+  it('[remove] should be able to delete a category', async () => {
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+    const expectedOutput = undefined;
+    const mockUpdateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    // @ts-ignore
+    controller['deleteCategoryUseCase'] = mockUpdateUseCase;
+    expect(controller.remove(id)).toBeInstanceOf(Promise);
+    const output = await controller.remove(id);
+    expect(mockUpdateUseCase.execute).toHaveBeenCalledWith({ id });
+    expect(expectedOutput).toStrictEqual(output);
   });
 });
--- src/nestjs/src/categories/dto/update-category.dto.ts
+++ src/nestjs/src/categories/dto/update-category.dto.ts
@@ -1,7 +1,8 @@
 import { UpdateCategoryUseCase } from '@cam/core/category/application';
 
-export class UpdateCategoryDto implements UpdateCategoryUseCase.Input {
-  id: string;
+export class UpdateCategoryDto
+  implements Omit<UpdateCategoryUseCase.Input, 'id'>
+{
   name: string;
   description?: string;
   is_active?: boolean;
